c4dcf7745f6fe59555f60b63dec4d3976dd32426
Bernardo So <bernard@onsite.com.ph>
2015-03-24 18:03:09 +0800

    Depot Scaffold Test

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.1}
+  validates :title, uniqueness: true
 end
